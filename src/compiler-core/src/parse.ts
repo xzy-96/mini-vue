661aa6c4883dd3fbcb63cdc4b2f766d7adba8c70
zhiyong.xu <zhiyong.xu@yuanchuanzhineng.com>
2022-07-06 22:33:27 +0800

    feat: 解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,13 +1,92 @@
-export function baseParse(str: string) {
+import { NodeTypes } from "./ast";
+const enum TagType {
+  START,
+  END,
+}
+export function baseParse(content: string) {
+  const context = createParseContext(content);
+
+  return crerateRoot(parseChildren(context));
+}
+function parseChildren(context) {
+  const nodes: any = [];
+  let node;
+  const s = context.source;
+  if (s.startsWith("{{")) {
+    node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    // 是否是 <div></div>
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
+  }
+
+  nodes.push(node);
+  return nodes;
+}
+
+function parseElement(context) {
+  const element = parseTag(context, TagType.START);
+  parseTag(context, TagType.END);
+  return element;
+}
+function parseTag(context, type: TagType) {
+  // 1.解析tag
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  console.log(match, "match");
+  const tag = match[1];
+  // 删除
+  advanceBy(context, match[0].length + 1);
+
+  console.log(context, "context");
+  if (type === TagType.END) return;
   return {
-    children: [
-      {
-        type: "interpolation",
-        conttent: {
-          type: "simple_expression",
-          content: "message",
-        },
-      },
-    ],
+    type: NodeTypes.ELEMENT,
+    tag: tag,
+  };
+}
+function parseInterpolation(context) {
+  // {{message}}
+
+  const openDelimiter = "{{";
+  const closeDelimiter = "}}";
+  // 获取 }} 的下标
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+  // 移动下标{{
+  advanceBy(context, openDelimiter.length);
+  // context.source = context.source.slice(openDelimiter.length);
+  console.log(context.source, "context.source ");
+  const rawContentLength = closeIndex - closeDelimiter.length;
+  // 提取值出来  {{值}}
+  const rawContent = context.source.slice(0, rawContentLength);
+  const content = rawContent.trim();
+  // 后面可能还有值
+  // context.source = context.source.slice(
+  //   rawContentLength + closeDelimiter.length
+  // );
+  advanceBy(context, rawContentLength + closeDelimiter.length);
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content + context.source,
+    },
+  };
+}
+
+function advanceBy(context, length: number) {
+  context.source = context.source.slice(length);
+}
+function crerateRoot(children) {
+  return {
+    children,
+  };
+}
+function createParseContext(content: string): any {
+  return {
+    source: content,
   };
 }
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -1,14 +1,27 @@
+import { NodeTypes } from "../src/ast";
 import { baseParse } from "../src/parse";
 
 describe("parse", () => {
   describe("interpolation", () => {
-    const ast = baseParse("{{message}}");
+    test("simple im", () => {
+      const ast = baseParse("{{ message}}");
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.INTERPOLATION,
+        content: {
+          type: NodeTypes.SIMPLE_EXPRESSION,
+          content: "message",
+        },
+      });
+    });
+  });
+});
+
+describe("element", () => {
+  it("simple element div", () => {
+    const ast = baseParse("<div></div>");
     expect(ast.children[0]).toStrictEqual({
-      type: "interpolation",
-      conttent: {
-        type: "simple_expression",
-        content: "message",
-      },
+      type: NodeTypes.ELEMENT,
+      tag: "div",
     });
   });
 });
